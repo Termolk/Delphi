141468b2820e57ff1d7b75617db6c87d40940cfa
Termolk <91014529+Termolk@users.noreply.github.com>
2023-02-28 12:00:44 +0300

    Full init

--- Jumping Button/Unit1.dfm
+++ Jumping Button/Unit1.dfm
@@ -1,9 +1,9 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
-  ClientHeight = 299
-  ClientWidth = 635
+  Caption = 'Jumping Button'
+  ClientHeight = 434
+  ClientWidth = 811
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,4 +13,13 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button: TButton
+    Left = 360
+    Top = 192
+    Width = 75
+    Height = 25
+    Caption = 'Click me'
+    TabOrder = 0
+    OnClick = ButtonClick
+  end
 end
--- Jumping Button/Unit1.pas
+++ Jumping Button/Unit1.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button: TButton;
+    procedure ButtonClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +22,18 @@ var
 implementation
 
 {$R *.dfm}
+
+
+
+procedure TForm1.ButtonClick(Sender: TObject);
+begin
+  Button.Width:= random(90)+10;
+  Button.Height:= random(90)+10;
+  Button.Top:= random(430);
+  Button.Left:= random(800);
+  if Button.Top+Button.Height>=600 then Button.Height:=600-Button.Top;
+ if Button.Left+Button.Width>=600 then Button.Width:=600-Button.Left;
+ Form1.Color:=random($FFFFFF);
+end;
 
 end.
